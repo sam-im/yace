cc89571e60f7aa89be64fe6a9d7ac42bfe5ccde3
Samim Umut Ökten <samim.oekten@unifr.ch>
2025-01-18 13:19:53 +0100

    Refactor utils

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,40 @@
 mod utils;
 
-use chip8::Chip8;
 use std::{cell::RefCell, rc::Rc};
+use chip8::Chip8;
 use wasm_bindgen::prelude::*;
 use web_sys::js_sys::Date;
 
 const SCALE: f64 = 1.;
+const TIME_PER_CYCLE: f64 = 0.01;
 
 #[wasm_bindgen(start)]
 fn main() -> Result<(), JsValue> {
-    utils::set_panic_hook(); // results in bigger size
-
+    utils::set_panic_hook();
     init_html()?;
     let mut chip8 = init_chip8()?;
 
-    const TIME_PER_CYCLE: f64 = 0.01;
-    let mut current_time = Date::now();
-    let mut accumulator = 0.0;
-
     let f = Rc::new(RefCell::new(None));
     let g = Rc::clone(&f);
+
+    let mut time = Date::now();
+    let mut acc = 0.;
+
     *g.borrow_mut() = Some(Closure::new(move || {
         let now = Date::now();
-        let dt = now - current_time;
-        current_time = now;
-        accumulator += dt;
+        let dt = now - time;
+        acc += dt;
+        time = now;
 
-        while accumulator >= TIME_PER_CYCLE {
+        while acc >= TIME_PER_CYCLE {
             chip8.cycle();
-            accumulator = accumulator - TIME_PER_CYCLE;
+            acc -= TIME_PER_CYCLE;
         }
 
         draw(&chip8.display);
-        request_animation_frame(f.borrow().as_ref().unwrap());
+        utils::request_animation_frame(f.borrow().as_ref().unwrap());
     }));
-    request_animation_frame(g.borrow().as_ref().unwrap());
-
+    utils::request_animation_frame(g.borrow().as_ref().unwrap());
     Ok(())
 }
 
@@ -48,17 +47,17 @@ fn init_chip8() -> Result<Chip8, JsValue> {
 }
 
 fn init_html() -> Result<(), JsValue> {
-    let elem = document().create_element("canvas")?;
+    let elem = utils::document().create_element("canvas")?;
     elem.set_id("canvas");
     let (width, height) = ((640. * SCALE) as usize, (320. * SCALE) as usize);
     elem.set_attribute("width", &width.to_string())?;
     elem.set_attribute("height", &height.to_string())?;
-    body().append_child(&elem)?;
+    utils::body().append_child(&elem)?;
     Ok(())
 }
 
 fn draw(display: &[bool]) {
-    let ctx = context();
+    let ctx = utils::context();
     let size = 10. * SCALE;
     display.iter().enumerate().for_each(|(i, val)| {
         let x = size * (i % 64) as f64;
@@ -69,47 +68,4 @@ fn draw(display: &[bool]) {
             ctx.fill_rect(x, y, size, size);
         }
     });
-}
-
-// Utility functions
-fn window() -> web_sys::Window {
-    web_sys::window().expect("no global `window` exists")
-}
-
-fn request_animation_frame(f: &Closure<dyn FnMut()>) {
-    window()
-        .request_animation_frame(f.as_ref().unchecked_ref())
-        .expect("should register `requestAnimationFrame` OK");
-}
-
-fn document() -> web_sys::Document {
-    window()
-        .document()
-        .expect("should have a document on window")
-}
-
-fn canvas() -> web_sys::HtmlCanvasElement {
-    document()
-        .get_element_by_id("canvas")
-        .expect("should have a canvas in body")
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .map_err(|_| ())
-        .unwrap()
-}
-fn context() -> web_sys::CanvasRenderingContext2d {
-    canvas()
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
-        .unwrap()
-}
-
-fn body() -> web_sys::HtmlElement {
-    document().body().expect("document should have a body")
-}
-
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,16 @@
 use wasm_bindgen::prelude::*;
 
+#[wasm_bindgen]
+extern "C" {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(a: &str);
+}
+
 pub fn set_panic_hook() {
     // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function at least once during initialization, and then
@@ -11,9 +22,39 @@ pub fn set_panic_hook() {
     console_error_panic_hook::set_once();
 }
 
+pub fn window() -> web_sys::Window {
+    web_sys::window().expect("no global `window` exists")
+}
 
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    pub fn log(a: &str);
+pub fn request_animation_frame(f: &Closure<dyn FnMut()>) {
+    window()
+        .request_animation_frame(f.as_ref().unchecked_ref())
+        .expect("should register `requestAnimationFrame` OK");
+}
+
+pub fn document() -> web_sys::Document {
+    window()
+        .document()
+        .expect("should have a document on window")
+}
+
+pub fn canvas() -> web_sys::HtmlCanvasElement {
+    document()
+        .get_element_by_id("canvas")
+        .expect("should have a canvas in body")
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| ())
+        .unwrap()
+}
+pub fn context() -> web_sys::CanvasRenderingContext2d {
+    canvas()
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap()
+}
+
+pub fn body() -> web_sys::HtmlElement {
+    document().body().expect("document should have a body")
 }
